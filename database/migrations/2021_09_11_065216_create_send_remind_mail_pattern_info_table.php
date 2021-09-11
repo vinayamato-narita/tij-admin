c5e6b8c495519128de9ca63dc572ece2c4a7cbc1
ティエン ゲン・バン <v-tien.nv@azpower.co.jp>
2021-09-11 14:12:44 +0700

    alter migration

---
+++ database/migrations/2021_09_11_065216_create_send_remind_mail_pattern_info_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSendRemindMailPatternInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('send_remind_mail_pattern_info', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->integer('send_remind_mail_pattern_id')->nullable();
+            $table->string('mail_subject')->nullable();
+            $table->text('mail_body')->nullable();
+            $table->string('lang_type', 10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('send_remind_mail_pattern_info');
+    }
+}

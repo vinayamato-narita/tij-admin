d83c861903687bebfb48231fb018587538e9650d
hiennd <hiennd@vinayamato.com>
2021-09-20 12:47:22 +0700

    seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,10 +23,9 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'admin_user_name' => $this->faker->name(),
-            'admin_user_email' => $this->faker->unique()->safeEmail(),
-            'admin_user_password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'is_sercurity' => Str::random(10),
+            'admin_name' => $this->faker->name(),
+            'admin_email' => $this->faker->unique()->safeEmail(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
     }
 

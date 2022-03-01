b3d1b19e1052e29cc2ea3f7c690475a6597f7fe8
QuyetNN <quyethd@vinayamato.com>
2022-03-01 14:12:07 +0700

    add sp lesson reserved

---
+++ database/migrations/2022_03_01_161030_alter_lesson_schedule_add_zoom_url.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterLessonScheduleAddZoomUrl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lesson_schedule', function (Blueprint $table) {
+            $table->string('zoom_url')->after('lesson_text_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

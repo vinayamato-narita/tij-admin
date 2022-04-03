7cfbaee44657112fd849fdd76ee2e654c5fc2f0f
nguyentrongmanh1998 <nguyentrongmanh050298@gmail.com>
2022-04-03 13:56:46 +0700

    fix default value

--- database/seeders/ZoomSettingSeeder.php
+++ database/seeders/ZoomSettingSeeder.php
@@ -18,8 +18,8 @@ class ZoomSettingSeeder extends Seeder
         DB::table('zoom_setting')->truncate();
 
         $zoomSetting = new ZoomSetting();
-        $zoomSetting->join_before_host = 1;
-        $zoomSetting->auto_recording = 1;
+        $zoomSetting->join_before_host = 0;
+        $zoomSetting->auto_recording = 0;
         $zoomSetting->waiting_room = 1;
         $zoomSetting->save();
     }

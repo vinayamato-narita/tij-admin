ca3be992878e30703eff3773c47f5ee78349c31f
ティエン ゲン・バン <v-tien.nv@azpower.co.jp>
2022-04-14 12:16:18 +0700

    tvij 286

--- database/seeders/FAQCategoryseeder.php
+++ database/seeders/FAQCategoryseeder.php
@@ -23,6 +23,20 @@ class FAQCategoryseeder extends Seeder
         DB::table('faq_parent_category')->insert(['faq_parent_category_id' => 1,
             'faq_parent_category_name' => 'コースの購入について']);
 
+        DB::table('faq_parent_category_info')->insert([
+            'faq_parent_category_info_id' => 1,
+            'faq_parent_category_id' => 1,
+            'faq_parent_category_name' => 'コースの購入について en',
+            'lang_type' => 'en'
+        ]);
+
+        DB::table('faq_parent_category_info')->insert([
+            'faq_parent_category_info_id' => 2,
+            'faq_parent_category_id' => 1,
+            'faq_parent_category_name' => 'コースの購入について zh',
+            'lang_type' => 'zh'
+        ]);
+
 
         DB::table('faq_category')->insert([
             'faq_category_id' => 1,
@@ -30,9 +44,35 @@ class FAQCategoryseeder extends Seeder
             'oder_number' => 1,
             'faq_category_name' => '3段階目のタイトル']);
 
+        DB::table('faq_category_info')->insert([
+            'faq_category_info_id' => 1,
+            'faq_category_id' => 1,
+            'faq_category_name' => '3段階目のタイトル en',
+            'lang_type' => 'en']);
+
+        DB::table('faq_category_info')->insert([
+            'faq_category_info_id' => 2,
+            'faq_category_id' => 1,
+            'faq_category_name' => '3段階目のタイトル zh',
+            'lang_type' => 'zh']);
+
 
         DB::table('faq_parent_category')->insert(['faq_parent_category_id' => 2,
             'faq_parent_category_name' => '講師について']);
+
+        DB::table('faq_parent_category_info')->insert([
+            'faq_parent_category_info_id' => 3,
+            'faq_parent_category_id' => 2,
+            'faq_parent_category_name' => '講師について en',
+            'lang_type' => 'en'
+        ]);
+
+        DB::table('faq_parent_category_info')->insert([
+            'faq_parent_category_info_id' => 4,
+            'faq_parent_category_id' => 2,
+            'faq_parent_category_name' => '講師について zh',
+            'lang_type' => 'zh'
+        ]);
 
 
         DB::table('faq_category')->insert([
@@ -41,6 +81,18 @@ class FAQCategoryseeder extends Seeder
             'oder_number' => 2,
             'faq_category_name' => '3段階目のタイトル']);
 
+        DB::table('faq_category_info')->insert([
+            'faq_category_info_id' => 3,
+            'faq_category_id' => 2,
+            'faq_category_name' => '3段階目のタイトル en',
+            'lang_type' => 'en']);
+
+        DB::table('faq_category_info')->insert([
+            'faq_category_info_id' => 4,
+            'faq_category_id' => 2,
+            'faq_category_name' => '3段階目のタイトル zh',
+            'lang_type' => 'zh']);
+
 
 
 
@@ -48,12 +100,39 @@ class FAQCategoryseeder extends Seeder
         DB::table('faq_parent_category')->insert(['faq_parent_category_id' => 3,
             'faq_parent_category_name' => '使用について']);
 
+        DB::table('faq_parent_category_info')->insert([
+            'faq_parent_category_info_id' => 5,
+            'faq_parent_category_id' => 3,
+            'faq_parent_category_name' => '使用について en',
+            'lang_type' => 'en'
+        ]);
+
+        DB::table('faq_parent_category_info')->insert([
+            'faq_parent_category_info_id' => 6,
+            'faq_parent_category_id' => 3,
+            'faq_parent_category_name' => '使用について zh',
+            'lang_type' => 'zh'
+        ]);
+
+
 
         DB::table('faq_category')->insert([
             'faq_category_id' => 3,
             'faq_parent_category_id' => 3,
             'oder_number' => 3,
             'faq_category_name' => '3段階目のタイトル']);
+
+        DB::table('faq_category_info')->insert([
+            'faq_category_info_id' => 5,
+            'faq_category_id' => 3,
+            'faq_category_name' => '3段階目のタイトル en',
+            'lang_type' => 'en']);
+
+        DB::table('faq_category_info')->insert([
+            'faq_category_info_id' => 6,
+            'faq_category_id' => 3,
+            'faq_category_name' => '3段階目のタイトル zh',
+            'lang_type' => 'zh']);
 
 
 

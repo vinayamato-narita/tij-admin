77ae5df546e02a8ab20298e35f707d42f2da6731
superxayda <dung.nt@vinayamato.com>
2022-04-27 09:58:53 +0700

    463

--- resources/js/components/course/add.vue
+++ resources/js/components/course/add.vue
@@ -743,7 +743,7 @@
                 this.messageText = "";
             },
             validateDepend(depend) {
-                this.$validator.validate(depend)
+              parseInt(this.minReserveCount)  > parseInt(this.maxReserveCount)?this.$validator.validate(depend): this.$validator.reset();
             }
         },
      }

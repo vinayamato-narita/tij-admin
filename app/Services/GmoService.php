8d29767fd5fe886772b128fe86db597178b18500
HTung <htung@vinayamato.com>
2022-04-29 14:19:04 +0700

    TIJV-468 Thêm xử lý quyết toán thanh toán vào batch group_course_decision

---
+++ app/Services/GmoService.php
@@ -0,0 +1,172 @@
+<?php
+/*
+    13.11.2019
+    GmoService.php
+*/
+
+namespace App\Services;
+use Log;
+
+class GmoService{
+
+    private $gmoURL;
+    private $shopID;
+    private $shopPass;
+
+    public function __construct(){
+        $this->gmoURL = config('gmo.url');
+        $this->shopID = config('gmo.shopID');
+        $this->shopPass = config('gmo.shopPass');
+    }
+
+    public function alterTran($jobCd, $accessID, $accessPass, $amount) {
+        $url = $this->gmoURL . "AlterTran.idPass";
+
+        // リクエストコネクションの設定
+        $curl=curl_init();
+        curl_setopt( $curl, CURLOPT_POST, true );
+        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $curl, CURLOPT_URL, $url );
+        $param = [
+            'ShopID'     => $this->shopID,
+            'ShopPass'   => $this->shopPass,
+            'AccessID'   => $accessID,
+            'AccessPass' => $accessPass,
+            'JobCd'      => $jobCd,
+            'Amount'     => $amount
+        ];
+        // リクエストボディの生成
+        curl_setopt( $curl, CURLOPT_POSTFIELDS, http_build_query( $param ) );
+
+        // リクエスト送信
+        $response = curl_exec( $curl );
+        $curlinfo = curl_getinfo( $curl );
+        curl_close( $curl );
+Log::info($response);
+        // レスポンスチェック
+        if( $curlinfo[ 'http_code' ] != 200 ){
+            // エラー
+
+            return false;
+        }
+
+        // レスポンスのエラーチェック
+        $dataMap = explode('&', $response);
+        $data = array();
+        foreach ($dataMap as $value) {
+            $splitArray = explode('=', $value, 2);
+            if (2 == count($splitArray)) {
+                $data[$splitArray[0]] = $splitArray[1];
+            }
+        }
+        if( array_key_exists( 'ErrCode', $data ) ){
+            // エラー
+
+            return false;
+        }
+
+        // 正常
+
+        return $data;
+    }
+
+    public function changeTran($jobCd, $accessID, $accessPass, $amount) {
+        $url = $this->gmoURL . "ChangeTran.idPass";
+
+        // リクエストコネクションの設定
+        $curl=curl_init();
+        curl_setopt( $curl, CURLOPT_POST, true );
+        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $curl, CURLOPT_URL, $url );
+        $param = [
+            'ShopID'     => $this->shopID,
+            'ShopPass'   => $this->shopPass,
+            'AccessID'   => $accessID,
+            'AccessPass' => $accessPass,
+            'JobCd'      => $jobCd,
+            'Amount'     => $amount
+        ];
+        // リクエストボディの生成
+        curl_setopt( $curl, CURLOPT_POSTFIELDS, http_build_query( $param ) );
+
+        // リクエスト送信
+        $response = curl_exec( $curl );
+        $curlinfo = curl_getinfo( $curl );
+        curl_close( $curl );
+
+        // レスポンスチェック
+        if( $curlinfo[ 'http_code' ] != 200 ){
+            // エラー
+
+            return false;
+        }
+
+        // レスポンスのエラーチェック
+        $dataMap = explode('&', $response);
+        $data = array();
+        foreach ($dataMap as $value) {
+            $splitArray = explode('=', $value, 2);
+            if (2 == count($splitArray)) {
+                $data[$splitArray[0]] = $splitArray[1];
+            }
+        }
+        if( array_key_exists( 'ErrCode', $data ) ){
+            // エラー
+
+            return false;
+        }
+
+        // 正常
+
+        return $data;
+    }
+
+    public function searchTrade($orderID) {
+        $url = $this->gmoURL . "SearchTrade.idPass";
+
+        // リクエストコネクションの設定
+        $curl=curl_init();
+        curl_setopt( $curl, CURLOPT_POST, true );
+        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $curl, CURLOPT_URL, $url );
+        $param = [
+            'ShopID'           => $this->shopID,
+            'ShopPass'         => $this->shopPass,
+            'OrderID'          => $orderID
+        ];
+        // リクエストボディの生成
+        curl_setopt( $curl, CURLOPT_POSTFIELDS, http_build_query( $param ) );
+
+        // リクエスト送信
+        $response = curl_exec( $curl );
+        $curlinfo = curl_getinfo( $curl );
+        curl_close( $curl );
+
+        // レスポンスチェック
+        if( $curlinfo[ 'http_code' ] != 200 ){
+            // エラー
+
+            return false;
+        }
+
+        // レスポンスのエラーチェック
+        $dataMap = explode('&', $response);
+        $data = array();
+        foreach ($dataMap as $value) {
+            $splitArray = explode('=', $value, 2);
+            if (2 == count($splitArray)) {
+                $data[$splitArray[0]] = $splitArray[1];
+            }
+        }
+        if( array_key_exists( 'ErrCode', $data ) ){
+            // エラー
+
+            return false;
+        }
+
+        // 正常
+
+        return $data;
+    }
+
+}
---
+++ config/gmo.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'url' => env('GMO_URL', 'null'),
+    'shopID' => env('GMO_SHOP_ID', 'null'),
+    'shopPass' => env('GMO_SHOP_PASSWORD', 'null'),
+    'accessID' => env('GMO_ACCESS_ID', 'null'),
+    'accessPass' => env('GMO_ACCESS_PASSWORD', 'null'),
+];

36723f8abdf13740d7627b283280e0912f733dbd
nguyentrongmanh1998 <nguyentrongmanh05021998@gmail.com>
2022-04-29 08:41:46 +0700

    select  zoom_personal_meeting_id

--- database/migrations/2022_04_29_103226_fix_sp_get_lesson_reserve_mail_select_zoom_personal_meeting_id.php
+++ database/migrations/2022_04_29_103226_fix_sp_get_lesson_reserve_mail_select_zoom_personal_meeting_id.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class FixSpGetLessonReserveMail extends Migration
+class FixSpGetLessonReserveMailSelectZoomPersonalMeetingId extends Migration
 {
     /**
      * Run the migrations.

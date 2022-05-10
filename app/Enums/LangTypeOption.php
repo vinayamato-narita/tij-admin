e591b75eb6a0fe45d371975e3326c8d753895ea6
superxayda <dung.nt@vinayamato.com>
2022-05-10 15:57:05 +0700

    552

--- app/Enums/LangTypeOption.php
+++ app/Enums/LangTypeOption.php
@@ -7,7 +7,7 @@ use BenSampo\Enum\Contracts\LocalizedEnum;
 
 final class LangTypeOption extends Enum implements LocalizedEnum
 {
-    const JAPANESE = 'jp';
+    const JAPANESE ='ja';
     const ENGLISH = 'en';
     const CHINESE = 'zh';
 }

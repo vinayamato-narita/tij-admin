35cc3d20a6d4224788e2a913d5ef199e451db49f
nguyentrongmanh1998 <nguyentrongmanh05021998@gmail.com>
2022-05-23 11:06:44 +0700

    fix procedure sp_get_course_detail_by_campaigncode, sp_get_course_detail_by_campaigncode_lms

---
+++ database/migrations/2022_05_23_121035_update_procedure_sp_get_course_detail_by_campaigncode.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateProcedureSpGetCourseDetailByCampaigncode extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $procedure = "
+        DROP PROCEDURE IF EXISTS `sp_get_course_detail_by_campaigncode`;
+        CREATE PROCEDURE `sp_get_course_detail_by_campaigncode`(
+        IN _course_id INT,
+        IN _lang_type VARCHAR(10))
+        BEGIN
+            SELECT course.course_id,
+                COALESCE(ci.course_name, course.course_name) as course_name,
+                COALESCE(ci.course_description, course.course_description) as course_description,
+                course.amount,
+                course.course_type,
+                course.expire_day,
+                course.min_reserve_count,
+                DATE_FORMAT(course.decide_date, '%Y/%m/%d %H:%i') as decide_date_show,
+                DATE_FORMAT(course.course_start_date, '%Y/%m/%d %H:%i') as course_start_date,
+                DATE_FORMAT(course.publish_date_to, '%Y/%m/%d %H:%i') as publish_date_to
+             FROM course
+             LEFT JOIN course_info ci ON ci.course_id = course.course_id and ci.lang_type = _lang_type
+            WHERE course.course_id = _course_id;
+        END
+        ";
+
+        \DB::unprepared($procedure);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

70a2393aa4b0e18e1576bf961dc8e1d69d80b82e
hiennd <hiennd@vinayamato.com>
2022-05-24 15:00:52 +0700

    TIJV-680

--- app/Enums/MailType.php
+++ app/Enums/MailType.php
@@ -17,4 +17,5 @@ final class MailType extends Enum
     const TEACHER_CANCEL_LESSON = 16;
     const MAIL_STUDENT_BEFORE_LESSON_START = 1;
     const MAIL_TEACHER_BEFORE_LESSON_START = 17;
+    const OPEN_GROUP_LESSON = 100;
 }

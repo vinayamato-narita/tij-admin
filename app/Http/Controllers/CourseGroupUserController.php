83ea004c39f83834fbbf12f280b95bbc4364c353
LinhTM <tran.manh.linh@cross-cloud.com>
2022-05-31 15:28:29 +0900

    TIJV-742 edit message

--- app/Http/Controllers/CourseGroupUserController.php
+++ app/Http/Controllers/CourseGroupUserController.php
@@ -524,7 +524,7 @@ class CourseGroupUserController extends BaseController
                         }
     
                         if (in_array($value['student_email'], $emails) == true) {
-                            $msg['error_list'] =  $value['student_email'] . 'すでに存在しています。';
+                            $msg['error_list'] =  $value['student_email'] . 'はすでに存在しています。';
                             break;
                         }
                         if ($value['student_name'] == null) {

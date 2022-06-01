a54422ccb84b0c44726f9f846fc651a0cba1a03b
superxayda <dung.nt@vinayamato.com>
2022-06-01 10:37:15 +0700

    fix509

--- resources/js/components/admin/edit-admin.vue
+++ resources/js/components/admin/edit-admin.vue
@@ -85,11 +85,11 @@
                                                     name="password"
                                                     v-model="adminInfoEx.password"
                                                     ref="password"
-                                                                     v-validate="{
-                            regex: regexMixin,
-                            required: true,
-                            max: 16,
-                            min : 8
+                                                    v-validate="{
+                                                        required: true,
+                                                        max: 16,
+                                                        min : 8,
+                                                        regex: regexMixin,
                                                 }"
                                                 />
                                                 <div
@@ -219,10 +219,10 @@ export default {
                     email: "メールアドレスを正確に入力してください"
                 },
                 password: {
-                   required: 'パスワードを入力してください。',
-                        max: 'パスワードは少なくとも、英字1字と数字1字を含む、記号を除く8字～16字の半角英数字で入力してください。',
-                        min: 'パスワードは少なくとも、英字1字と数字1字を含む、記号を除く8字～16字の半角英数字で入力してください。',
-                        regex: 'パスワードは少なくとも、英字1字と数字1字を含む、8字～16字の半角英数字または記号で入力してください。'
+                    required: 'パスワードを入力してください。',
+                    max: 'パスワードは16文字以内で入力してください。',
+                    min: 'パスワードは8文字以上で入力してください。',
+                    regex: 'パスワードは少なくとも、英字1字と数字1字を含む、8字～16字の半角英数字または記号で入力してください。'
                 },
                 password_confirm: {
                     confirmed: "確認が一致しません"

eeaae32bfbd219e15b847d8f3e041bc1bf6ef8a5
HTung <htung@vinayamato.com>
2022-06-01 17:31:05 +0700

    TIJV-574 【学習者】登録情報変更時に419エラーが表示される/【Student site】Khi change thông tin đăng ký, show màn lỗi 419

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof \Illuminate\Session\TokenMismatchException){
+            if ($request->expectsJson()) {
+                return response()->json(['error' => 'Unauthenticated.'], 401);
+            }
+
+            return redirect('/');
+        }
         return parent::render($request, $exception);
     }
 }

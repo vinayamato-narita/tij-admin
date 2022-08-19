5d844f8daa871e6d3fdd79e8b38697d0980063f9
HTung <htung@vinayamato.com>
2022-08-19 11:12:45 +0700

    TIJV-925 グループコース成立条件不具合

--- app/Console/Commands/GroupCourseDecision.php
+++ app/Console/Commands/GroupCourseDecision.php
@@ -67,12 +67,21 @@ class GroupCourseDecision extends Command
 
         foreach ($courses as $course) {
             try {
-                $reserveNum = $course->pointSubscriptionHistories->count();
+                // remove duplicated pointSubscriptionHistory
+                $pointSubscriptionHistories = [];
+                foreach ($course->pointSubscriptionHistories as $p) {
+                    if (empty($pointSubscriptionHistories[$p->student_id."_".$p->course_id])) {
+                        $pointSubscriptionHistories[] = $p;
+                    }
+                }
+
+                // calculate reserved students
+                $reserveNum = $pointSubscriptionHistories->count();
 
                 if ($reserveNum < $course->min_reserve_count) {
                     $cancelCourseIds[] = $course->course_id;
 
-                    foreach ($course->pointSubscriptionHistories as $pointSubscriptionHistory) {
+                    foreach ($pointSubscriptionHistories as $pointSubscriptionHistory) {
                         // cancel order
                         try {
                             Log::info("cancel order id:".$pointSubscriptionHistory->order_id);
@@ -143,7 +152,7 @@ class GroupCourseDecision extends Command
                 } else {
                     $decideCourseIds[] = $course->course_id;
 
-                    foreach ($course->pointSubscriptionHistories as $pointSubscriptionHistory) {
+                    foreach ($pointSubscriptionHistories as $pointSubscriptionHistory) {
                         // update sales order
                         try {
                             Log::info("update sales order id:".$pointSubscriptionHistory->order_id);
